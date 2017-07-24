1189d66867db30e05b7582b1559f27a738f7fd89
Tita <nimch681@student.otago.ac.nz>
2017-07-24 12:10:17 +1200

    New project on local branch

---
+++ HololensReceiver/Assets/ResetWorld.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetWorld : MonoBehaviour {
+
+    public void OnSelect()
+    {
+        UnityEngine.VR.InputTracking.Recenter();
+        //GameObject pointRenderer = GameObject.Find("PointCloudRenderer");
+        // Destroy(pointRenderer);
+        // SceneManager.LoadScene("scene");
+    }
+}
